b6c5c0c7ef24de8874615ccaae965a0d910815b2
Santushti 26 <santushtiranjan@gmail.com>
2022-07-27 20:49:04 +0530

    o

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -7,6 +7,7 @@ export default function Menu(props) {
 
 
     <motion.div initial={{opacity:"0"}} animate={{y:[-1000,-100], opacity:"1"}} exit={{y:[-100,1000]}} className="menuBody">
+        
         <motion.div className="menuContainer">
             <div className="textBox">
                 <motion.div animate={{x:[-2800,0]}} exit={{x:[0,-2800]}} transition={{duration: 1}} className='text'>
---
+++ src/valoRegistration.js
@@ -0,0 +1,506 @@
+import React from 'react'
+import Navbar from './Components/Navbar';
+import { useState } from 'react';
+import AnimatedRoutes from './AnimatedRoutes';
+import FrontPage from './Components/FrontPage';
+//import valo from "./Assets/valo.png";
+import './valoImg.css';
+import './valo.png';
+import { useInView } from 'react-intersection-observer';
+
+
+
+
+
+export default function Registration() {
+    const [menu,setMenu] = useState(0);
+    const { ref, inView} = useInView({
+      /* Optional options */
+      threshold: 0,
+      
+      
+
+
+    });
+
+    const [visible, setVisible]=useState(false);
+
+    
+    
+
+  return (
+    //<div className>Registration</div>
+    // <div>
+    //   <div className='navibar'>
+    //     <Navbar setMenu={setMenu}/>
+    //     <AnimatedRoutes></AnimatedRoutes>
+    //   </div>
+
+      
+        
+    //       <div className={inView?'valoTextContainer valoTextContainer--zoom':'valoTextContainer'} ref={ref}>
+    //         <div className='valopic'>
+
+    //         <div className='frntpg'>
+
+              
+    //           <div className='maintxt'>
+    //             <div className='valoText'>
+    //               VALORANT
+    //             </div>
+
+    //             <div className='regTypetxt' style={{width: "100vw"}}>
+    //               <div>
+    //                 Registration Type
+    //               </div>
+    //               <div>
+    //                 <form>
+    //                   <div style={{margin: "20px 0"}}>
+    //                     <label style={{padding: "0 10px 0 0"}}>
+    //                       <input type="radio" name="players" style={{margin: "0 2px 0 0"}}/>
+    //                       Solo Entry
+    //                     </label>
+    //                     <label >
+    //                       <input type="radio" name="players" style={{margin: "0 2px 0 0"}}/>
+    //                       Team Entry
+    //                     </label>
+    //                   </div>
+    //                 </form>
+    //                 <form>
+    //                   <label>
+    //                     Name
+    //                     <div>
+    //                       <input type="text" name="name"/>
+    //                     </div>
+    //                   </label>
+    //                   <label>
+    //                     Name
+    //                     <div>
+    //                       <input type="text" name="name"/>
+    //                     </div>
+    //                   </label>
+    //                   <label>
+    //                     Name
+    //                     <div>
+    //                       <input type="text" name="name"/>
+    //                     </div>
+    //                   </label>
+    //                 </form>
+    //               </div>
+    //             </div>
+                
+
+
+                
+
+    //           </div>
+              
+
+    //         </div>
+
+            
+
+            
+
+    //       </div>
+
+          
+           
+    //     </div>
+
+        
+        
+        
+        
+        
+        
+        
+      
+      
+      
+
+
+        
+    // </div>
+
+    
+    <div>
+
+      
+        
+        <div className='navibar'>
+          <Navbar setMenu={setMenu}/>
+          <AnimatedRoutes></AnimatedRoutes>
+        </div>
+
+        {/* <div className={inView?'valoTextContainer valoTextContainer--zoom':'valoTextContainer'} ref={ref}> */}
+
+         <div class="valoTextContiner">
+
+        
+
+      <div class="container">
+			  <img id="valo" src="./Assets/valo.png"/>
+			  <div class="divos">
+				    <div>VALORANT</div>
+            
+			  </div>
+        
+        
+			  {/* <img id="icon" src="./Assets/icon.png"/> */}
+        
+		  </div>
+
+      
+
+      <br></br>
+      <br></br>
+      <br></br>
+
+      
+
+
+      
+
+      <div class="bigdiv">
+      <br></br>
+        <br/>
+        <div class="registrationTypeSection">
+        <form>
+        <div style={{margin: "20px 0"}}>
+            <label style={{padding: "0 10px 0 0"}}>
+              <span id='registrationType'>Registration Type</span><br/><br/>
+              <input type="radio" name="players" style={{margin: "0 2px 0 0"}} onClick={ ()=>setVisible(false)}/>
+              Solo Entry
+            </label>
+          <label >
+              <input type="radio" name="players" style={{margin: "0 2px 0 0"}} onClick={ ()=>setVisible(true)}/>
+              Team Entry
+            </label>
+          </div>
+        </form>
+        </div>
+      
+			<div>
+			<br/>
+      <div class="player1">
+      <ul id="player">
+       <li id='number'>
+       <span id='playerNum'>1</span>
+       </li>
+       <li>
+       <div class="textBoxes"> 
+        
+        <div>
+          <br/>
+
+          Name:<br/><br/>
+          <input type="text" value="" placeholder="Your Name"/>
+        </div>
+        
+        <div>
+        <br/>
+          Registration no.:<br/><br/>
+          <input type="text" value="" placeholder="Your Registration no. ..."/>
+        </div>
+        <div>
+        <br/>
+          Contact no.:<br/><br/>
+          <input type="text" value="" placeholder="Your Contact no. ..."/>
+        </div>
+        <div>
+        <br/>
+          VIT Email ID:<br/><br/>
+          <input type="text" value="" placeholder="Your VIT Email ID ..."/>
+        </div>
+        <div>
+        <br/>
+          Discord ID:<br/><br/>
+          <input type="text" value="" placeholder="Your Discord ID ..."/>
+        </div>
+        <div>
+        <br/>
+          Riot ID:<br/><br/>
+          <input type="text" value="" placeholder="Your Riot ID ..."/>
+        </div>
+
+        </div>
+        </li>
+        </ul>
+
+        </div>
+
+        
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        {visible &&
+
+        <div class="teamReg">
+
+        <div class="player2">
+
+        
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+       <ul id="player">
+       <li id='number'>
+       <span id='playerNum'>2</span>
+       </li>
+       <li>
+       <div class="textBoxes"> 
+        
+        <div>
+          <br/>
+          Name:<br/><br/>
+          <input type="text" value="" placeholder="Your Name"/>
+        </div>
+        
+        <div>
+        <br/>
+          Registration no.:<br/><br/>
+          <input type="text" value="" placeholder="Your Registration no. ..."/>
+        </div>
+        <div>
+        <br/>
+          Contact no.:<br/><br/>
+          <input type="text" value="" placeholder="Your Contact no. ..."/>
+        </div>
+        <div>
+        <br/>
+          VIT Email ID:<br/><br/>
+          <input type="text" value="" placeholder="Your VIT Email ID ..."/>
+        </div>
+        <div>
+        <br/>
+          Discord ID:<br/><br/>
+          <input type="text" value="" placeholder="Your Discord ID ..."/>
+        </div>
+        <div>
+        <br/>
+          Riot ID:<br/><br/>
+          <input type="text" value="" placeholder="Your Riot ID ..."/>
+        </div>
+
+        
+      
+
+        </div>
+        </li>
+        </ul>
+        </div>
+
+        <div class="player3">
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+<ul id="player">
+<li id='number'>
+<span id='playerNum'>3</span>
+</li>
+<li>
+<div class="textBoxes"> 
+
+<div>
+  <br/>
+  Name:<br/><br/>
+  <input type="text" value="" placeholder="Your Name"/>
+</div>
+
+<div>
+<br/>
+  Registration no.:<br/><br/>
+  <input type="text" value="" placeholder="Your Registration no. ..."/>
+</div>
+<div>
+<br/>
+  Contact no.:<br/><br/>
+  <input type="text" value="" placeholder="Your Contact no. ..."/>
+</div>
+<div>
+<br/>
+  VIT Email ID:<br/><br/>
+  <input type="text" value="" placeholder="Your VIT Email ID ..."/>
+</div>
+<div>
+<br/>
+  Discord ID:<br/><br/>
+  <input type="text" value="" placeholder="Your Discord ID ..."/>
+</div>
+<div>
+<br/>
+  Riot ID:<br/><br/>
+  <input type="text" value="" placeholder="Your Riot ID ..."/>
+</div>
+
+
+
+
+</div>
+</li>
+</ul>
+</div>
+
+<div class="player4">
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+<ul id="player">
+<li id='number'>
+<span id='playerNum'>4</span>
+</li>
+<li>
+<div class="textBoxes"> 
+
+<div>
+  <br/>
+  Name:<br/><br/>
+  <input type="text" value="" placeholder="Your Name"/>
+</div>
+
+<div>
+<br/>
+  Registration no.:<br/><br/>
+  <input type="text" value="" placeholder="Your Registration no. ..."/>
+</div>
+<div>
+<br/>
+  Contact no.:<br/><br/>
+  <input type="text" value="" placeholder="Your Contact no. ..."/>
+</div>
+<div>
+<br/>
+  VIT Email ID:<br/><br/>
+  <input type="text" value="" placeholder="Your VIT Email ID ..."/>
+</div>
+<div>
+<br/>
+  Discord ID:<br/><br/>
+  <input type="text" value="" placeholder="Your Discord ID ..."/>
+</div>
+<div>
+<br/>
+  Riot ID:<br/><br/>
+  <input type="text" value="" placeholder="Your Riot ID ..."/>
+</div>
+
+
+
+
+</div>
+</li>
+</ul>
+</div>
+
+<div class="player5">
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+<ul id="player">
+<li id='number'>
+<span id='playerNum'>5</span>
+</li>
+<li>
+<div class="textBoxes"> 
+
+<div>
+  <br/>
+  Name:<br/><br/>
+  <input type="text" value="" placeholder="Your Name"/>
+</div>
+
+<div>
+<br/>
+  Registration no.:<br/><br/>
+  <input type="text" value="" placeholder="Your Registration no. ..."/>
+</div>
+<div>
+<br/>
+  Contact no.:<br/><br/>
+  <input type="text" value="" placeholder="Your Contact no. ..."/>
+</div>
+<div>
+<br/>
+  VIT Email ID:<br/><br/>
+  <input type="text" value="" placeholder="Your VIT Email ID ..."/>
+</div>
+<div>
+<br/>
+  Discord ID:<br/><br/>
+  <input type="text" value="" placeholder="Your Discord ID ..."/>
+</div>
+<div>
+<br/>
+  Riot ID:<br/><br/>
+  <input type="text" value="" placeholder="Your Riot ID ..."/>
+</div>
+
+
+
+
+</div>
+</li>
+</ul>
+</div>
+
+<br/>
+<br/>
+<br/>
+
+
+        </div>
+}
+
+        </div>
+		 </div> 
+
+    </div>
+
+    </div>
+
+    
+    
+    
+    
+    
+  )
+}
+export default App;
