7c66c151c87bdd9780dc19fb811790f1c99c6a37
Santushti 26 <108174335+ECEop@users.noreply.github.com>
2022-09-14 01:08:12 +0530

    :)

--- src/AppEventsVGL.js
+++ src/AppEventsVGL.js
@@ -49,9 +49,9 @@ export default function AppEventsVGL() {
 
     },
     {
-      heading : "Rocket League",
+      heading : "CODM",
       images : imagesRegistrations,
-      registration_link : "/rocketLeagueRegistration",
+      registration_link : "/codmRegistration",
       bracket_link : "",
 
     },
@@ -70,9 +70,31 @@ export default function AppEventsVGL() {
     <motion.div animate={{y:[-1000,0]}} className="eventVGL">
     <Navbar setMenu ={setMenu}/>
   
-      <FrontPage text1="VELLORE INSTITUTE OF TECHNOLOGY" heading="VGL" text2="We are the premium gaming and technology club of Vellore Institute of Technology.
-      To us, technology is a goal and gaming a religion."/>
-      <AboutText heading="ABOUT THE EVENT" text="We are the premium gaming and technology club of Vellore Institute of Technology. To us, technology is a goal and gaming a religion. Following our tagline, the club hosts two major gaming events namely Game-a-thon and VIT Gamers league along with a numerous workshops on game development, web-development, UI-UX, design and Artificial intelligence.While our gaming events witness thousands of game enthusiasts every year, putting in a lot of hardwork to win our humongous prize pools.Our technical workshops are attended by exuberant participants with a zeal to learn and often with the aim to be a part of our core club. Through Our latest venture TAG PROJECTS we aim to undertake projects on the latest techWe and also host annual Game development workshop Studio-2-Stage where participants can learn to design their games from real world professionals.- THE TEAM"/>
+      <FrontPage heading="RECON" text2="Initiale | Elevate."/>
+      <AboutText heading="ABOUT THE EVENT" text="THE MAIN IDEA OF INITIATE & ELEVATE IS TO START OFF THE EVENT AND ELEVATE IT TO TAKE IT TO THE NEXT LEVEL. THE EVENT WILL BE FOR ALL-INDIA COLLEGE STUDENTS. IT WILL BE CONDUCTED OFFLINE IN VIT ON 30TH SEP-2ND OCT 2022 IN VIT VELLORE CAMPUS.WE PLAN TO CONDUCT THE EVENT IN TWO PHASES, PHASE 1 BEING A GAME.DEVELOPMENT HACKATHON AND PHASE 2 BEING AN ALL INDIA ESPORTS TOURNAMENT. THE DETAILS ARE AS FOLLOWS:-
+
+STUDIO-TO-STAGE (GAME DEV HACKATHON OR GAMEUAM) - IT IS A
+48HRS CONTINUOUS GAME DEVELOPMENT . HACKATHON WHERE
+PARTICIPANTS WILL BE GIVEN A BASIC THEME (ON THE SPOT) WITH CERTAIN
+CONDITIONS AND THEY HAVE TO DEVELOP PC 2D/3D GAMES BASED ON THE
+THEME ON ANY GAME DEVELOPMENT ENGINE THEY MAY WISH TO. THE GAMES:
+WILL BE PUBLISHED ON TAG CLUB'S, STUDIO TO STAGE, ITCH.IO PAGE. THE
+SUBMITTED GAMES WILL THEN BE JUDGED BASED ON MULTIPLE CRITERIA
+NAMELY RELEVANCE TO THE THEME, PLAYABILITY, IDEA, ETC. THE FIRST 3
+PODIUM HOLDERS WILL BE REWARDED.
+
+GAME-A-THON (GAT) - THIS IS TAG'S ALL INDIA ESPORTS TOURNAMENT.
+CONDUCTED EVERY YEAR. THIS YEAR WE WILL BE CONDUCTING GAMES LIKE:
+CSGO, VALORANT, FIFA 22 AND ROCKET LEAGUE. THE REGISTRATIONS.
+WILL BE TAKEN IN USING TAG'S OFFICIAL WEBSITE. (CURRENTLY NO ENTRY
+FEES). PLAYERS CAN PARTICIPATE EITHER WITH THEIR TEAMS OR SOLO.
+FOR SOLO PARTICIPANTS, WE WILL BE ALLOTTING. TEAMS BASED ON THEIR ;
+
+ 
+
+RANKS. THE MATCHMAKING WILL BE DONE ACCORDINGLY. THE WINNERS ANI
+RUNNER UPS OF EACH GAME WILL BE REWARDED.
+"/>
       {/* <RegistrationCarousel heading="VALORANT" images={imagesRegistrations}/> */}
       {
         events.map((element,idx)=>{
