90fac17c7b63ecdaa21b2d3a53727863f7cdb2a3
ShamitrM <shamitr.mardikar@gmail.com>
2022-09-15 22:49:21 +0530

    Push Request 2
    Change of Carousel Hover Effect, Anything Pushed has been done by @rishiluniya if anything he shall be executed, Thanks <3

---
+++ src/testworkshops.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Footer from './Components/Footer';
+import AboutText from './Components/AboutText';
+import FrontPage from './Components/FrontPage';
+
+export default function Workshops(){
+    
+}
