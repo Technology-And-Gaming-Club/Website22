3751cebd87e28f9dfe70afa555d16381e2a0edb6
Santushti 26 <91973710+Santushti26@users.noreply.github.com>
2022-09-24 16:45:40 +0530

    p

--- src/Home.js
+++ src/Home.js
@@ -28,7 +28,7 @@ export default function Home() {
   return (
     <motion.div animate={{y:[-1000,0]}}>
       <Navbar setMenu={setMenu}/>
-      <FrontPage text1="VELLORE INSTITUTE OF TECHNOLOGY" heading="TECHNOLOGY AND GAMING CLUB" text2="We are the premium gaming and technology club of Vellore Institute of Technology.
+      <FrontPage text1="VELLORE INSTITUTE OF TECHNOLOGY" heading="TAG CLUB" text2="We are the premium gaming and technology club of Vellore Institute of Technology.
       To us, technology is a goal and gaming a religion."/>
       <AboutText heading="ABOUT US" text="We are the premium gaming and technology club of Vellore Institute of Technology. To us, technology is a goal and gaming a religion. Following our tagline, the club hosts two major gaming events namely Game-a-thon and VIT Gamers league along with a numerous workshops on game development, web-development, UI-UX, design and Artificial intelligence.While our gaming events witness thousands of game enthusiasts every year, putting in a lot of hardwork to win our humongous prize pools.Our technical workshops are attended by exuberant participants with a zeal to learn and often with the aim to be a part of our core club. Through Our latest venture TAG PROJECTS we aim to undertake projects on the latest techWe and also host annual Game development workshop Studio-2-Stage where participants can learn to design their games from real world professionals.- THE TEAM"/>
         
