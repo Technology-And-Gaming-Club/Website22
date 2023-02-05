11529d8e19cfbb0e777b5a432fcae4ca1f928b72
NeelOommen <neelittyerahoommen@gmail.com>
2023-02-05 21:34:09 +0530

    Temporary Commit
    Bare working level

--- src/Components/AboutText.js
+++ src/Components/AboutText.js
@@ -13,19 +13,19 @@ export default function AboutText(props) {
   return (
     <div className={inView?"aboutContainer aboutContainer--zoom":"aboutContainer"} ref={ref}>
 
-    <div className='aboutText'>
-      <motion.div  className="leftAbout">
-        <div className="leftAboutHeading">
-            {props.heading}
-        </div>
-        <div className="leftAboutText">
-            {props.text}
-        </div>
-      </motion.div>
-      <motion.div className="rightAbout">
-        <img src="Assets/aboutImage.png"/>
-      </motion.div>
-    </div>
+      <div className='aboutText'>
+        <motion.div  className="leftAbout">
+          <div className="leftAboutHeading">
+              {props.heading}
+          </div>
+          <div className="leftAboutText">
+              {props.text}
+          </div>
+        </motion.div>
+        <motion.div className="rightAbout">
+          <img src="Assets/aboutImage.png"/>
+        </motion.div>
+      </div>
     </div>
   )
 }
